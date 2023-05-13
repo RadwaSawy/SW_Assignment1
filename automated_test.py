3a82b7b710ae2d2a2115066decbf4fbd028643b0
RadwaSawy <100291466+RadwaSawy@users.noreply.github.com>
2023-05-13 21:32:44 +0300

    Update automated_test.py

--- automated_test.py
+++ automated_test.py
@@ -4,7 +4,7 @@ from unittest.mock import patch
 from math_ops import add_numbers
 
 class TestAddNumbers(unittest.TestCase):
-    def test_add_numbers(self):
+    def automated_test(self):
         # Redirect stdout to a buffer
         with patch('sys.stdout', new=StringIO()) as buffer:
             # Call the add_numbers function with the arguments 2 and 3
